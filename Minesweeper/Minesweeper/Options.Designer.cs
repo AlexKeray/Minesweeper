ac9bd15e23f22d460f2766446ccdef42e35c424b
AlexKeray <alex.keray@gmail.com>
2023-05-31 11:17:06 +0300

    Adding the 3 forms

--- Minesweeper/Minesweeper/Options.Designer.cs
+++ Minesweeper/Minesweeper/Options.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Options
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Name = "Options";
             this.Text = "Options";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
